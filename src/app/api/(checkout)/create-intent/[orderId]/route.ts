58a453442bf3e0944fc47b75ec0d1b5ab4539dd2
Ramin <raminxanifmuslim@gmail.com>
2024-02-12 05:57:18 +0400

    asdf

--- src/app/api/(checkout)/create-intent/[orderId]/route.ts
+++ src/app/api/(checkout)/create-intent/[orderId]/route.ts
@@ -4,40 +4,39 @@ import { NextRequest, NextResponse } from "next/server";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(
-  request: NextRequest,
-  { params }: { params: { orderId: string } }
+    request: NextRequest,
+    { params }: { params: { orderId: string } }
 ) {
-  const { orderId } = params;
+    const { orderId } = params;
 
-  const order = await prisma.order.findUnique({
-    where: {
-      id: orderId,
-    },
-  });
-
-  if (order) {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: order.price * 100,
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
+    const order = await prisma.order.findUnique({
+        where: {
+            id: orderId,
+        },
     });
+    if (order) {
+        const amount = Number(order.price) * 100;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: "usd",
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        });
 
-    await prisma.order.update({
-      where: {
-        id: orderId,
-      },
-      data: { intent_id: paymentIntent.id },
+        await prisma.order.update({
+            where: {
+                id: orderId,
+            },
+            data: { intent_id: paymentIntent.id },
+        });
+
+        return new NextResponse(
+            JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+            { status: 200 }
+        );
+    }
+    return new NextResponse(JSON.stringify({ message: "Order not found!" }), {
+        status: 404,
     });
-
-    return new NextResponse(
-      JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-      { status: 200 }
-    );
-  }
-  return new NextResponse(
-    JSON.stringify({ message:"Order not found!" }),
-    { status: 404 }
-  );
 }
