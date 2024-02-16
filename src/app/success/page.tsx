2daf2c7eb7247f6a9fd8e6bdf35b778e5be2aa7c
Ramin <raminxanifmuslim@gmail.com>
2024-02-16 04:40:47 +0400

    sadf

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { API_BASE } from "@/constants";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import ConfettiExplosion from "react-confetti-explosion";
 
 const SuccessPage = () => {
@@ -11,6 +11,11 @@ const SuccessPage = () => {
 
     useEffect(() => {
         const makeRequest = async () => {
+            if (!payment_intent) {
+                console.error("Payment intent not found.");
+                return;
+            }
+
             try {
                 await fetch(`${API_BASE}/api/confirm/${payment_intent}`, {
                     method: "PUT",
@@ -19,7 +24,8 @@ const SuccessPage = () => {
                     router.push("/orders");
                 }, 5000);
             } catch (err) {
-                console.log(err);
+                console.error("Error confirming payment:", err);
+                // Показать пользователю сообщение об ошибке или предпринять другие действия
             }
         };
 
@@ -27,15 +33,17 @@ const SuccessPage = () => {
     }, [payment_intent, router]);
 
     return (
-        <>
-            <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
-                <p className="max-w-[600px]">
-                    Payment successful. You are being redirected to the orders
-                    page. Please do not close the page.
-                </p>
-                <ConfettiExplosion className="absolute m-auto" />
-            </div>
-        </>
+        <Suspense fallback={<div>Loading...</div>}>
+            <>
+                <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
+                    <p className="max-w-[600px]">
+                        Payment successful. You are being redirected to the
+                        orders page. Please do not close the page.
+                    </p>
+                    <ConfettiExplosion className="absolute m-auto" />
+                </div>
+            </>
+        </Suspense>
     );
 };
 
